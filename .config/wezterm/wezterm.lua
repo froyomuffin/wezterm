04b4feba0d897eeafd106dc41b76353c5c7b3459
Tom <froyomuffin@gmail.com>
2024-12-19 16:44:22 -0500

    Update some configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+wezterm.on('format-window-title', function () return 'Wezterm' end)
+
 config.color_scheme = 'nightfox'
 
 -- This is where you actually apply your config choices
@@ -16,12 +18,18 @@ config.colors = {
   selection_bg = '#FFFACD',
 }
 
-config.font_size = 11.0
+-- Don't confirm quit
+config.window_close_confirmation = 'NeverPrompt'
+
+config.font_size = 10.0
+config.font = wezterm.font_with_fallback { 'Noto Sans Mono' }
+-- config.warn_about_missing_glyphs = false
 
 config.window_background_opacity = 0.95
 
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 config.tab_bar_style = {
 }
